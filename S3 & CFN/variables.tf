8e7f94e3581cac5a0dbb958f2561ec7497fa947d
amavrogiannis <alex.mavrogiannis@gmail.com>
2022-05-25 01:45:02 +0100

    s3 n cfn

--- S3 & CFN/variables.tf
+++ S3 & CFN/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_id" {
   description = "Give a name to the S3 Bucket"
   type        = string
-  default     = "test.alexmav.co.uk"
+  default     = "cv.alexmav.co.uk"
 }
 
 variable "bucket_env_tag" {
@@ -16,18 +16,21 @@ variable "bucket_env_tag" {
 variable "bucket_group_tag" {
   description = "Specify your tags [cv/test/www]"
   type        = string
-  default     = "wwww"
+  validation {
+    condition     = contains(["cv", "test", "www"], var.bucket_group_tag)
+    error_message = "You have to only specify one of the following >> cv/test/www"
+  }
 }
 
 variable "acm_certificate_arn" {
   description = "Paste the ARN Certificate you created on AWS ACM"
   type        = string
-  default     = "arn:aws:acm:us-east-1:582037776064:certificate/1bb0bf47-9c2c-4030-9b17-5a6d3d317a93"
+  default     = "arn:aws:acm:us-east-1:582037776064:certificate/44b3e5f4-b8d1-4a3d-b585-2f00733a53f4"
 }
 
 variable "aws_cfn_comment" {
   description = "Enter your CloudFront comment here"
   type        = string
-  default     = "N/A"
+  default     = "Personal deployment"
 }
 
