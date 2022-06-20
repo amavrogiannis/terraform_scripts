dfc7212f4bda1be2b9d1b2616ce4f4553ba23283
and-amavrogiannis <alexander.mavrogiannis@and.digital>
2022-06-20 12:22:34 +0100

    route53 file

--- S3 & CFN/Route53.tf
+++ S3 & CFN/Route53.tf
@@ -22,21 +22,27 @@ resource "aws_route53_record" "sub_domain_record" {
 }
 
 resource "aws_route53_record" "cert_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  # for_each = {
+  #   for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+  #     name   = dvo.resource_record_name
+  #     record = dvo.resource_record_value
+  #     type   = dvo.resource_record_type
+  #   }
+  # }
 
+  # allow_overwrite = true
+  # name            = each.value.name
+  # records         = [each.value.record]
+  # ttl             = 60
+  # type            = each.value.type
+  # zone_id         = aws_route53_zone.alexmavcouk.zone_id
+  count = length(aws_acm_certificate.cert.domain_validation_options)
+  name = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
+  type = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
+  zone_id = aws_route53_zone.alexmavcouk.zone_id
+  records = [ element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index) ]
+  ttl = var.dns_record_ttl
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.alexmavcouk.zone_id
 
   lifecycle {
     prevent_destroy = true
